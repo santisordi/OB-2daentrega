174173e753516e5ba35cc338105779af50becd61
Santiago Sordi <santiagosordi@gmail.com>
2023-03-06 18:38:51 -0300

    2da entrega

---
+++ segunda-entrega.js
@@ -0,0 +1,14 @@
+const soyDesarrollador = true;
+const fechaNacimiento = new Date ("november 20 1984");
+const libroFavorito ={
+    titulo: 'El Hobit', 
+    autor: 'J.J.R Tolkien', 
+    fecha: 1964,
+    url: 'https://www.planetadelibros.com.ar/libro-el-hobbit/290283'
+}
+
+
+const misDatos = ['Santiago Sordi', 38, soyDesarrollador, fechaNacimiento, libroFavorito]
+
+console.log(misDatos)
+
